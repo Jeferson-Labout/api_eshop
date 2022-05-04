80489c9fd61d281183b3ed045621646fd4e6a7d7
Jeferson Labout <Jefson1989@gmail.com>
2022-05-04 14:59:23 -0300

    API FULL

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-//Middleware
-app.use(bodyParser.json())
-app.use(morgan('tiny'))
-
-const api = process.env.API_URL
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use(errorHandler);
 
 //Routes
-const categoriesRoutes = require('./routers/categories');
-const productsRoutes = require('./routers/products');
-const usersRoutes = require('./routers/users');
-const ordersRoutes = require('./routers/orders');
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
+const api = process.env.API_URL;
 
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-app.use(`${api}/categories`, categoriesRoutes)
-app.use(`${api}/products`, productsRoutes)
-app.use(`${api}/users`, usersRoutes)
-app.use(`${api}/orders`, ordersRoutes)
-
-
-
-
+//Database
 mongoose
-  .connect(process.env.CONNECTION_STRING)
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
   .then(() => {
-    console.log('A conexão do banco de dados está pronta')
+    console.log("A conexão do banco de dados está pronta...");
   })
-  .catch(err => {
-    console.log(err)
-  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+//Server
 app.listen(3000, () => {
-  console.log('Servidor em execução http://localhost:3000')
-})
+  console.log("Servidor em execução http://localhost:3000");
+});
